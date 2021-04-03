7d648076beacd7aeee1377caca7d73b8a251ff79
Rodrigo Carvalho <rcf2504@gmail.com>
2021-04-03 16:44:15 -0300

    Primeiro commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # lab2-sist-dist-ufrj
 Laboratório 2 da disciplina de Sistemas Distribuídos da UFRJ
+
+O objetivo deste Laboratório é desenvolver uma aplicação distribuída básica para aplicar os conceitos estudados sobre arquitetura de software em camada e arquitetura de sistema centralizada (cliente/servidor); e seguir praticando com a programação usando sockets.
+
+A aplicação que vamos desenvolver consiste em contar as ocorrências das palavras em um arquivo texto.
+
+• Entrada: usuário informa o nome do arquivo texto.
+• Saída (com sucesso): a aplicação exibe a lista das 10 palavras mais encontradas no arquivo, ordenadas da mais frequente para a menos frequente, e o número de ocorrências de cada palavra.
+• Saída (com erro): informa que o arquivo solicitado não foi encontrado.
+
+Proposta de arquitetura de sistema:
+
+1. Lado cliente: implementa a camada de interface com o usuário. O usuário poderá solicitar o processamento de um ou mais arquivos em uma única execução da aplicação: o programa espera pelo nome do arquivo, faz o processamento, retorna o resultado, e então aguarda um novo pedido de arquivo ou o comando de finalização.
+
+2. Lado servidor: implementa a camada de processamento e a camada de acesso aos dados. Implemente um servidor iterativo, isto é, que trata as requisições 
+de um cliente de cada vez, em um único fluxo de execução. Terminada a interação com um cliente, ele poderá voltar a esperar por nova conexão. Dessa forma, o programa do servidor fica em loop infinito.
---
+++ servidor.py
@@ -0,0 +1,75 @@
+import socket as sock
+from collections import Counter
+
+HOST = '' # '' possibilita acessar qualquer endereco alcancavel da maquina local
+PORT = 5000 # porta onde chegarao as mensagens para essa aplicacao
+
+NUMBER_OF_WORDS = 10 # numero de palavras mais encontradas desejado
+
+# cria um socket para comunicacao
+serverSocket = sock.socket() # valores default: socket.AF_INET, socket.SOCK_STREAM  
+
+# vincula a interface e porta para comunicacao
+serverSocket.bind((HOST,PORT))
+
+# define o limite maximo de conexoes pendentes e coloca-se em modo de espera por conexao
+serverSocket.listen(1)
+
+while True:
+
+    print('O servidor esta pronto para receber conexoes')
+
+    # aceita a primeira conexao da fila (chamada pode ser BLOQUEANTE)
+    connectionSocket, address = serverSocket.accept() # retorna um novo socket e o endereco do par conectado
+
+    # imprime o par (IP,PORTA) da conexao 
+    print('Conectado com: ', address)
+
+    # depois de conectar-se, espera uma mensagem (chamada pode ser BLOQUEANTE)
+    receivedMsg = connectionSocket.recv(2048) # argumento indica a qtde maxima de dados
+
+    while receivedMsg:
+
+        # transforma a mensagem recebida em bytes para string
+        receivedMsgString = str(receivedMsg,encoding='utf-8')
+
+        # imprime a mensagem recebida
+        print('Mensagem recebida: ', receivedMsgString)
+
+        # acrescenta o formato .txt no fim do nome do arquivo caso necessario
+        if not receivedMsgString.endswith('.txt'):
+            receivedMsgString += '.txt'
+
+        try:
+            # le o arquivo de nome igual a mensagem recebida
+            with open(receivedMsgString, 'rt') as txtFile:
+                
+                # instancia um dicionario do tipo Counter a partir da lista de palavras contidas no arquivo
+                # onde cada par (chave, valor) representa uma palavra e o numero de vezes que ela aparece
+                counter = Counter(txtFile.read().split())
+
+                # pega a lista das n palavras mais frequentes do dicionario, onde n é NUMBER_OF_WORDS
+                mostCommonWords = counter.most_common(NUMBER_OF_WORDS)
+
+                # transforma a lista de palavras mais frequentes em string
+                msgToSend = str(mostCommonWords)
+
+                # envia a lista de palavras mais frequentes para o cliente
+                connectionSocket.send(bytes(msgToSend, encoding='utf-8'))
+
+        # caso um arquivo com o nome requisitado nao seja encontrado, envia mensagem de erro para o cliente
+        except FileNotFoundError:
+            msgToSend = 'ERRO - Arquivo nao encontrado!'
+            connectionSocket.send(bytes(msgToSend, encoding='utf-8'))
+            
+        # imprime a mensagem enviada
+        print('Mensagem enviada: ', msgToSend)
+        
+        # espera uma nova mensagem (chamada pode ser BLOQUEANTE)
+        receivedMsg = connectionSocket.recv(2048) # argumento indica a qtde maxima de dados
+    
+    # fecha o socket da conexao
+    connectionSocket.close()
+    
+    # imprime mensagem de conexao encerrada
+    print('Conexao encerrada com: ', address)
